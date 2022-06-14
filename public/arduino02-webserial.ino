bf9825d408e06080e656d8b4b08e6491e6a01321
Jeremy Ellis <keyfreemusic@gmail.com>
2022-06-13 19:07:18 -0700

    Update arduino02-webserial.ino

--- public/arduino02-webserial.ino
+++ public/arduino02-webserial.ino
@@ -1,13 +1,13 @@
 byte incomingByte[1];
-int myDelay = 2000;   // non-block delay in milliseconds
+int myDelay = 3000;   // non-block delay in milliseconds
 unsigned long myStart; 
-bool  mySwitch = false; 
 int serialMessage = 1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   myStart = millis();   // set delay
+  randomSeed(analogRead(A0));  // AO pin on XIAO does not have to be connected to anything
 
 }
 
@@ -15,11 +15,13 @@ void loop() {
 
     if ( (millis() - myStart ) >= myDelay) {       
        myStart = millis();      //  reset the delay time
-       Serial.write(serialMessage); 
+       byte myRandByte = (byte)random(0, 8);
+       Serial.write(myRandByte); 
      }
 
   
   if (Serial.available() > 0) {
+   // incomingByte[1] = Serial.read();
     serialMessage = Serial.parseInt();
     if(serialMessage == 1) {
       Serial.println("LED On");
@@ -32,4 +34,3 @@ void loop() {
 }
 
   
-
